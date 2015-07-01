188f4be8b41b38acdbaa43b76bbea17eb182882c
jhegg <josh.hegg@gmail.com>
2015-06-30 21:14:50 -0700

    Switching from restify to express
    404 errors were not being handled by restify or swagger, and I spent too
    much time trying to figure it out. They work out of the box with
    express, and it was really easy to switch over; just some minor changes
    to app.js, and dependency changes to package.json.

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 'use strict';
 
-var SwaggerRestify = require('swagger-restify-mw');
-var restify = require('restify');
-var app = restify.createServer();
-
+var SwaggerExpress = require('swagger-express-mw');
+var express = require('express');
+var app = express();
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
 
-SwaggerRestify.create(config, function(err, swaggerRestify) {
+// need to handle the index page before registering swagger, or swagger
+// takes it over
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/static/index.html')
+});
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
-  swaggerRestify.register(app);
+  // install middleware
+  swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
 
   console.log('App now listening on port: ' + port);
 });
+
+// catch-all route for static resources
+app.use('/', express.static(__dirname + '/static'));
